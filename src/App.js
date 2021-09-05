afa77fcfeef9c1686a7b7008e4eb3e7b1f2d0334
Matt B <mbaranich@yahoo.com>
2021-09-05 19:34:19 -0400

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState(["Todo 1", "Todo 2"]);
+	return (
+		<>
+			<TodoList todos={todos} />
+			<input type="text" />
+			<button>Add Todo</button>
+			<button>Clear Completed Todos</button>
+			<div>0 left to do</div>
+		</>
+	);
 }
 
 export default App;
